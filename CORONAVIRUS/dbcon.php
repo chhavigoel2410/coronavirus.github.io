8874aa49ffd7c22d642d01abb8c3a79326e8fc0f
chhavigoel2410 <89148002+chhavigoel2410@users.noreply.github.com>
2021-08-18 21:28:18 +0530

    my corona project

---
+++ CORONAVIRUS/dbcon.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername ="localhost";
+$username ="root";
+$password ="";
+$dbname ="crona";
+
+$conn = mysqli_connect($servername,$username,$password,$dbname );
+if($conn)
+{
+//    echo "connection made";
+}
+?>
